480f0f202d3123de988b9b0f5f80b3668e0594b6
shubham9237 <l.shubham9237@gmail.com>
2025-09-27 22:02:52 +0530

    [SL] Fixed the cache issue using new model

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
-
+const redis = require("redis");
+const util = require("util");
+const redisUrl='redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log("Iam about run a queru");
-    return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function() {
+    const key = JSON.stringify(Object.assign({} , this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+
+    const cacheValue = await client.get(key);
+    if(cacheValue){
+        const doc = JSON.parse(cacheValue);
+        return Array.isArray(doc) 
+        ? doc.map(elm => new this.model(elm))
+        : new this.model(doc);
+        
+        // need to handle single object and array of abojects
+    }
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result))
+    return result;
 }
