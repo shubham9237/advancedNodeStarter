ade72a08b5f3883741c2d7fe2f478e87e4f8040d
shubham9237 <l.shubham9237@gmail.com>
2025-09-28 17:46:40 +0530

    [SL] Added cache js and modified prototyp of mongoose

--- services/cache.js
+++ services/cache.js
@@ -1,23 +1,31 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
-const redisUrl='redis://127.0.0.1:6379';
+const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = async function() {
-    const key = JSON.stringify(Object.assign({} , this.getQuery(), {
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true;
+    return this;
+}
+
+mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
 
     const cacheValue = await client.get(key);
-    if(cacheValue){
+    if (cacheValue) {
         const doc = JSON.parse(cacheValue);
-        return Array.isArray(doc) 
-        ? doc.map(elm => new this.model(elm))
-        : new this.model(doc);
-        
+        return Array.isArray(doc)
+            ? doc.map(elm => new this.model(elm))
+            : new this.model(doc);
+
         // need to handle single object and array of abojects
     }
     const result = await exec.apply(this, arguments);
