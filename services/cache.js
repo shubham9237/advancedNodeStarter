3a6ff18c281d6763fd592107341813ff119dc7fc
shubham9237 <l.shubham9237@gmail.com>
2025-09-28 18:13:07 +0530

    [SL] Added the user id as the main key for nexted data

--- services/cache.js
+++ services/cache.js
@@ -3,11 +3,12 @@ const redis = require("redis");
 const util = require("util");
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
     return this;
 }
 
@@ -19,17 +20,20 @@ mongoose.Query.prototype.exec = async function () {
         collection: this.mongooseCollection.name
     }));
 
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
     if (cacheValue) {
+        console.log("from cache");
         const doc = JSON.parse(cacheValue);
         return Array.isArray(doc)
             ? doc.map(elm => new this.model(elm))
             : new this.model(doc);
 
-        // need to handle single object and array of abojects
+        // need to handle single object and array of objects
     }
+    console.log("NOT from cache");
+
     const result = await exec.apply(this, arguments);
 
-    client.set(key, JSON.stringify(result))
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
     return result;
 }
